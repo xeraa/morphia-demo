370304cfc8b283cdf3a0d7fb579ea9b8bf101589
Philipp Krenn <pk@xeraa.net>
2014-11-16 18:10:05 +0100

    MINOR cleanup of the generic persistence test

--- src/test/java/net/xeraa/morphia_demo/test/GenericPersistenceTest.java
+++ src/test/java/net/xeraa/morphia_demo/test/GenericPersistenceTest.java
@@ -6,8 +6,6 @@ import net.xeraa.morphia_demo.entities.WorkerEntity;
 import org.bson.types.ObjectId;
 import org.junit.Test;
 
-import java.math.BigDecimal;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -27,8 +25,7 @@ public class GenericPersistenceTest extends BaseTest {
     assertNotNull("An ObjectId should have been generated when saving the entity", id1);
     assertEquals("The return value and actual value of the ObjectId should match",
 		 company.getId(), id1);
-    WorkerEntity worker = new WorkerEntity().setFirstname("firstname").setSurname("surname")
-        .setEmail("foo@bar.com").setSalary(new BigDecimal("5.25")).setYearsExperience(5);
+    WorkerEntity worker = new WorkerEntity().setFirstname("firstname");
     ObjectId id2 = genericPersistence.persist(worker);
     assertEquals("The return value and actual value of the ObjectId should match",
 		 worker.getId(), id2);
@@ -43,13 +40,11 @@ public class GenericPersistenceTest extends BaseTest {
 		 genericPersistence.count(CompanyEntity.class));
     assertEquals("In a clean database there should be zero entries", 0,
 		 genericPersistence.count(WorkerEntity.class));
-    CompanyEntity company1 = new CompanyEntity().setName("Test company 1")
-        .setWeb("http://www.test1.com").setEmail("foobar@test1.com");
+    CompanyEntity company1 = new CompanyEntity().setName("Test company 1");
     genericPersistence.persist(company1);
     assertEquals("After adding an entity there should be one entry", 1,
 		 genericPersistence.count(CompanyEntity.class));
-    CompanyEntity company2 = new CompanyEntity().setName("Test company 2")
-        .setWeb("http://www.test2.com").setEmail("foobar@test2.com");
+    CompanyEntity company2 = new CompanyEntity().setName("Test company 2");
     genericPersistence.persist(company2);
     assertEquals("After adding another entity there should be two entries", 2,
 		 genericPersistence.count(CompanyEntity.class));
@@ -62,12 +57,14 @@ public class GenericPersistenceTest extends BaseTest {
    */
   @Test
   public void getById() {
-    assertNull("An invalid ObjectId should return null",
-	       genericPersistence.get(CompanyEntity.class, new ObjectId()));
     CompanyEntity company = new CompanyEntity().setName("Test company");
     ObjectId id = genericPersistence.persist(company);
-    assertNotNull("An should be available under the given ObjectId",
+    assertNull("An invalid ObjectId should return null",
+	       genericPersistence.get(CompanyEntity.class, new ObjectId()));
+    assertNotNull("An entity should be available under the given ObjectId",
 		  genericPersistence.get(CompanyEntity.class, id));
+    assertEquals("The entity should be the one of the returned ObjectId", "Test company",
+                 genericPersistence.get(CompanyEntity.class, id).getName());
     assertNull("A valid ObjectId for a different collection should return null",
 	       genericPersistence.get(WorkerEntity.class, id));
   }
